67394101d3d492f2008b75798d4b84851856aa3f
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-02-03 13:52:39 +0100

    RFID File edited: Status -- Versuch mit RFID 3 Pyserial und UART;

--- files/HardwareDevelopment/RFID.py
+++ files/HardwareDevelopment/RFID.py
@@ -1,29 +1,53 @@
+import serial
 import time
-import board
-import busio
-from adafruit_pn532.uart import PN532_UART
 
-# Initialisierung der UART-Schnittstelle
-uart = busio.UART(board.TX, board.RX, baudrate=115200, timeout=1)
+# Serielle Verbindung öffnen
+# Prüfe, ob dein Modul /dev/ttyS0 oder /dev/ttyAMA0 verwendet
+ser = serial.Serial('/dev/ttyS0', baudrate=115200, timeout=1)
 
-# PN532 über UART initialisieren
-nfc = PN532_UART(uart, debug=False)
 
-# Firmware-Version auslesen
-ic, ver, rev, support = nfc.firmware_version
-print(f'Gefunden PN532 mit Firmware Version: {ver}.{rev}')
+def wakeup():
+    # Wake-up Sequence für PN532 (abhängig vom Modul)
+    ser.write(b'\x55\x55\x00\x00\x00\x00')  # Testbefehl, je nach Modul
+    time.sleep(0.5)
 
-# NFC-Reader konfigurieren
-nfc.SAM_configuration()
 
-print('Warte auf eine NFC-Karte...')
+def send_command(command):
+    preamble = b'\x00\x00\xFF'  # Preamble für PN532
+    length = bytes([len(command)])
+    lcs = bytes([~len(command) & 0xFF])  # Length Checksum
+    data = command
+    dcs = bytes([~sum(command) & 0xFF])  # Data Checksum
+    postamble = b'\x00'
 
-while True:
-    uid = nfc.read_passive_target(timeout=0.5)
+    frame = preamble + length + lcs + data + dcs + postamble
+    ser.write(frame)
 
-    if uid is None:
-        continue  # Kein Tag erkannt, weitersuchen
 
-    # UID der Karte anzeigen
-    print('Karte erkannt! UID:', ' '.join([hex(i) for i in uid]))
+def read_response():
+    response = ser.read(64)  # Lese bis zu 64 Bytes
+    if response:
+        print("Antwort:", response.hex())
+    else:
+        print("Keine Antwort vom PN532.")
+
+
+def read_card():
+    # Befehl zum Lesen einer NFC-Karte (InListPassiveTarget)
+    command = b'\xD4\x4A\x01\x00'
+    send_command(command)
     time.sleep(1)
+    read_response()
+
+
+if __name__ == "__main__":
+    try:
+        wakeup()
+        print("Warte auf eine NFC-Karte...")
+        while True:
+            read_card()
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("Programm beendet.")
+    finally:
+        ser.close()
