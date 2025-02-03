cb013200824b738fead70aff69a9dfb9ccdfc60b
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-02-03 12:08:04 +0100

    Harware File edited: Status --Lamp will be go on! Änderung neu

--- files/HardwareDevelopment/main.py
+++ files/HardwareDevelopment/main.py
@@ -5,16 +5,16 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # Definiere den Pin 17 als Ausgang
-GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.OUT)
 
 # Schalte den Pin 17 auf HIGH
-GPIO.output(17, GPIO.HIGH)
+GPIO.output(27, GPIO.HIGH)
 
 # Optional: Warte etwas, damit der HIGH-Zustand sichtbar ist
 time.sleep(2)
 
 # Setze den Pin wieder auf LOW (optional)
-GPIO.output(17, GPIO.LOW)
+GPIO.output(27, GPIO.LOW)
 
 # Aufräumen, um die Pins wieder freizugeben
 GPIO.cleanup()
