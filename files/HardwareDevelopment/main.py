1b0e733a240866b4d6c72658e8eb3438f25c989d
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-02-03 12:09:17 +0100

    Harware File edited: Status --Lamp will be go on! Änderung neu - Funktioniert LED

--- files/HardwareDevelopment/main.py
+++ files/HardwareDevelopment/main.py
@@ -4,16 +4,12 @@ import time
 # GPIO-Nummerierung nach Broadcom-Layout verwenden (BCM)
 GPIO.setmode(GPIO.BCM)
 
-# Definiere den Pin 17 als Ausgang
 GPIO.setup(27, GPIO.OUT)
 
-# Schalte den Pin 17 auf HIGH
 GPIO.output(27, GPIO.HIGH)
 
-# Optional: Warte etwas, damit der HIGH-Zustand sichtbar ist
 time.sleep(2)
 
-# Setze den Pin wieder auf LOW (optional)
 GPIO.output(27, GPIO.LOW)
 
 # Aufräumen, um die Pins wieder freizugeben
