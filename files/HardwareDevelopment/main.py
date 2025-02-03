ac81748b7e3ec89b826a0892e4f7dacb76c83b5d
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-02-03 11:54:26 +0100

    Harware File edited: Status --Lamp will be go on!

---
+++ files/HardwareDevelopment/main.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO
+import time
+
+# GPIO-Nummerierung nach Broadcom-Layout verwenden (BCM)
+GPIO.setmode(GPIO.BCM)
+
+# Definiere den Pin 18 als Ausgang
+pin11 = GPIO.setup(17, GPIO.OUT)
+
+pin11.on(GPIO.HIGH
