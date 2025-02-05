1465e5fc608ada8bc475ac36dcd0bfb62e61f3a9
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-02-05 09:54:49 +0100

    fertiges file test.py uploaded kompletter relaycode enthalten

--- files/HardwareDevelopment/test.py
+++ files/HardwareDevelopment/test.py
@@ -3,16 +3,32 @@ from time import sleep
 
 
 
-relay1 = OutputDevice(23, active_high=True, initial_value=False)
-relay2 = OutputDevice(24, active_high=True, initial_value=False)
 
 
-relay1.on()
-relay2.on()
-print("relay1 IN1 on")
-print("relay2 IN2 on")
-sleep(5)
-print("relay1 IN1 stoppt")
-print("relay2 IN2 stoppt")
-relay1.off()
-relay2.off()
+
+
+def run(parameter):
+    zeiteinzeln = 50
+    zeitgesamt = 25
+    relay1 = OutputDevice(23, active_high=True, initial_value=False)
+    relay2 = OutputDevice(24, active_high=True, initial_value=False)
+    if parameter == "Cola":
+        relay1.on()
+        print("Cola läuft")
+        sleep(zeiteinzeln)
+        relay1.off()
+        print("Cola stoppt")
+    elif parameter == "Fanta":
+        relay2.on()
+        print("Fanta läuft")
+        sleep(zeiteinzeln)
+        relay2.off()
+        print("Fanta stoppt")
+    elif parameter == "Mezzo":
+        relay1.on()
+        relay2.on()
+        print("Mezzo läuft - Beide Relays an")
+        sleep(zeitgesamt)
+        relay1.off()
+        relay2.off()
+        print("Mezzo stoppt - beide beendet")
