ce2358133e4808fc7a9b5e2bca9e39c4c9653ba2
FeldherrWeiprecht <drag.moser@outlook.com>
2025-02-06 09:59:13 +0100

    update AI more precise now

--- files/AIDevelopment/ai.py
+++ files/AIDevelopment/ai.py
@@ -17,14 +17,17 @@ def detect_blue():
         diff = cv2.absdiff(prev_gray, gray)
         motion_detected = np.sum(diff > 25) > 5000
         prev_gray = gray
-        lower_blue = np.array([80, 150, 70])
+        
+        lower_blue = np.array([35, 50, 50])
         upper_blue = np.array([100, 255, 255])
+        
         mask = cv2.inRange(hsv, lower_blue, upper_blue)
         blue_percentage = (np.sum(mask > 0) / (frame.shape[0] * frame.shape[1])) * 100
         cv2.putText(frame, f'Blau: {blue_percentage:.2f}%', (10, 50),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
         cv2.imshow('Frame', frame)
         cv2.imshow('Maske', mask)
+        
         if blue_percentage >= 40:
             if motion_detected:
                 timer_start = None
@@ -33,11 +36,14 @@ def detect_blue():
                     timer_start = time.time()
                 elif time.time() - timer_start >= 3:
                     print("end")
-                    return 1;
+                    return 1
                     timer_start = None
         else:
             timer_start = None
+            
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+            
     cap.release()
     cv2.destroyAllWindows()
+detect_blue()
