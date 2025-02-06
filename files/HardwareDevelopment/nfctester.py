2667f8920ecee58efa468da31a5c8ce1fd32bf3c
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-02-06 15:28:12 +0100

    harware autostart finsihed

--- files/HardwareDevelopment/nfctester.py
+++ files/HardwareDevelopment/nfctester.py
@@ -1,9 +1,15 @@
-import nfc
+import serial
 
-clf = nfc.ContactlessFrontend('/dev/serial0')
+# Öffne die serielle Verbindung mit 115200 Baud
+ser = serial.Serial('/dev/ttyS0', 115200, timeout=1)
 
-if clf:
-    print("PN532 erkannt!")
-    clf.close()
-else:
-    print("Fehler beim Erkennen des PN532")
+# Sende einen Befehl an den PN532 (GetFirmwareVersion)
+get_firmware = bytes([0x00, 0x00, 0xFF, 0x04, 0xFC, 0xD4, 0x02, 0x2A, 0x00])
+ser.write(get_firmware)
+
+# Lese Antwort
+response = ser.read(16)
+print("Antwort vom PN532:", response.hex())
+
+# Schließe Verbindung
+ser.close()
