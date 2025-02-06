16f9cbfe042ad8e829514e424703848cfe23c7ee
codeengineer2 <maximilian.zangerl@student.htl-rankweil.at>
2025-02-06 11:40:25 +0100

    harware autostart finsihed

--- files/HardwareDevelopment/main.py
+++ files/HardwareDevelopment/main.py
@@ -4,8 +4,8 @@ from time import sleep
 
 relay1 = OutputDevice(23, active_high=True, initial_value=False)
 relay2 = OutputDevice(24, active_high=True, initial_value=False)
-relay1.off()
-relay2.off()
+relay1.on()
+relay2.on()
 
 
 
@@ -14,24 +14,24 @@ def run(parameter):
     zeitgesamt = 10 # 33
 
     if parameter == "Cola":
-        relay1.on()
+        relay1.off()
         print("Cola läuft")
         sleep(zeiteinzeln)
-        relay1.off()
+        relay1.on()
         print("Cola stoppt")
     elif parameter == "Fanta":
-        relay2.on()
+        relay2.off()
         print("Fanta läuft")
         sleep(zeiteinzeln)
-        relay2.off()
+        relay2.on()
         print("Fanta stoppt")
     elif parameter == "Spezi":
-        relay1.on()
-        relay2.on()
-        print("Spezi läuft - Beide Relays an")
-        sleep(zeitgesamt)
         relay1.off()
         relay2.off()
+        print("Spezi läuft - Beide Relays an")
+        sleep(zeitgesamt)
+        relay1.on()
+        relay2.on()
         print("Spezi stoppt - beide beendet")
 
 
