8c37f545be95e77eecb790b0ec462234fa2ffcab
FeldherrWeiprecht <drag.moser@outlook.com>
2025-02-07 09:50:44 +0100

    update hardware main file

--- files/HardwareDevelopment/main.py
+++ files/HardwareDevelopment/main.py
@@ -4,31 +4,32 @@ from time import sleep
 
 relay1 = OutputDevice(23, active_high=True, initial_value=False)
 relay2 = OutputDevice(24, active_high=True, initial_value=False)
-relay1.off()
-relay2.off()
+relay1.on()
+relay2.on()
 
 
 def run(parameter):
-    zeiteinzeln = 60 # 65
+    zeiteinzeln = 50 # 65
     zeitgesamt = 30 # 33
 
     if parameter == "Cola":
-        relay1.on()
+        relay1.off()
         print("Cola läuft")
         sleep(zeiteinzeln)
-        relay1.off()
+        relay1.on()
         print("Cola stoppt")
     elif parameter == "Fanta":
-        relay2.on()
+        relay2.off()
         print("Fanta läuft")
         sleep(zeiteinzeln)
-        relay2.off()
+        relay2.on()
         print("Fanta stoppt")
     elif parameter == "Spezi":
-        relay1.on()
-        relay2.on()
-        print("Spezi läuft - Beide Relays an")
-        sleep(zeitgesamt)
         relay1.off()
         relay2.off()
+        print("Spezi läuft - Beide Relays an")
+        sleep(zeitgesamt)
+        relay1.on()
+        relay2.on()
         print("Spezi stoppt - beide beendet")
+        
